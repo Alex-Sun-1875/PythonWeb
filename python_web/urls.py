44870ea11c87b0cf4ad1901137bdd804ea4ad7bf
sunlh <200359659@qq.com>
2019-03-14 23:59:10 +0800

    add template & inherit

--- python_web/urls.py
+++ python_web/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import view
+
+"""
+Django 的 path() 函数可以接收四个参数,分别是两个必选参数: route, view 和两个可选参数: kwargs, name
+route: 字符串,表示 url 规则,与之匹配的是 url 会执行对应的第二个参数 view
+view: 用于执行与正则表达式匹配的 url 请求
+kwargs: 视图使用的字典类型的参数
+name: 用来反向获取 URL
+"""
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hello/', view.hello),
 ]
